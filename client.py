a9e6d2cc75f45d3e7f6753cd866a8134ab95ab84
1Zarkos1 <zarkos777@gmail.com>
2022-11-19 20:00:11 +0530

    added non blocking input and message receive capability from server

--- client.py
+++ client.py
@@ -2,6 +2,31 @@ import asyncio
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
+message_queue = asyncio.Queue()
+
+async def get_message():
+    while True:
+        message = await asyncio.to_thread(input, 'Input your message: ')
+        await message_queue.put(message)
+        await asyncio.sleep(0.1)
+
+async def send_message(writer):
+    while True:
+        message = await message_queue.get()
+        message_queue.task_done()
+        writer.write(message.encode('utf-8'))
+        await writer.drain()
+
+async def read_message(reader):
+    while True:
+        message = await reader.read(50)
+        if message := message.decode('utf-8'):
+            print()
+            print(message)
+        else:
+            print('disconnecting from server')
+            break
+
 async def client_connection():
     try:
         server_address = ('127.0.0.52', 5200)
@@ -10,10 +35,10 @@ async def client_connection():
         print(f"Could not connect to specified server at {server_address}")
         return
     try:
-        while True:
-            message = input('Input your message: ')
-            writer.write(message.encode('utf-8'))
-            await writer.drain()
+        input_task = asyncio.create_task(get_message())
+        read = asyncio.create_task(read_message(reader))
+        write = asyncio.create_task(send_message(writer))
+        await asyncio.gather(read, write, input_task)
     except KeyboardInterrupt:
         writer.close()
         await writer.wait_closed()
