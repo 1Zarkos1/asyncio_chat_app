47b1002caa16049b216839b7a446627d95d2d893
1Zarkos1 <zarkos777@gmail.com>
2022-11-19 19:57:54 +0530

    added message distribution functionality

--- server.py
+++ server.py
@@ -1,40 +1,42 @@
 import asyncio
-from datetime import datetime
 
-connected_clients = 0
+connected_clients = {}
 
 async def handle_connection(reader, writer):
     client = writer.get_extra_info('peername')
-    await log_action('connected to', client)
+    await log_action('connected to', client, writer)
     try:
         while True:
             message = await reader.read(50)
             if message := message.decode('utf-8'):
-                print(f'{client} send: {message}')
+                await distribute_message(client, message, connected_clients.values())
             else:
                 break
     except ConnectionResetError as e:
         print(f'Connection to {client} was lost')
-    await log_action('left', client)
+    await log_action('left', client, writer)
     writer.close()
     await writer.wait_closed()
 
-async def log_action(action, client):
-    global connected_clients
+async def log_action(action, client, writer):
     message = f"Client {client} {action} the server"
     if action == 'connected to':
-        connected_clients += 1 
+        connected_clients.update({client: writer}) 
     else:
-        connected_clients -= 1
+        del connected_clients[client]
     print(message)
-    print(f"Members online: {connected_clients}")
+    print(f"Members online: {len(connected_clients)}")
+
+async def distribute_message(sender, message, receivers):
+    message = f'{sender} send: {message}'
+    print(message)
+    for receiver in receivers:
+        receiver.write(message.encode('utf-8'))
+        await receiver.drain()
 
 async def server_work():
     server = await asyncio.start_server(handle_connection, '127.0.0.52', 5200)
     async with server:
         await server.serve_forever()
-
-async def main():
-    await server_work()
 
 asyncio.run(server_work())
